8f9f3923196d8ca21a5e741d9e0d4575f3bd03ee
Emi <183315760+cahexis@users.noreply.github.com>
2025-04-19 21:10:35 -0500

    Create LuckyBlockPlacement.server.lua

---
+++ ServerScriptService/LuckyBlockPlacement.server.lua
@@ -0,0 +1,19 @@
+local rep = game:GetService("ReplicatedStorage")
+local buildings = rep.Buildings
+local placecube = rep.PlaceCube
+local cube = rep.PlacedCube
+local debounce = false
+placecube.OnServerEvent:Connect(function(player, rootpos)
+	local backpack = player:WaitForChild("Backpack")
+	local character = player.Character
+	if character:FindFirstChildWhichIsA("Tool") then
+		character:FindFirstChildWhichIsA("Tool"):Destroy()
+	end
+	if debounce == false then
+		debounce = true
+		local dupecube = cube:Clone()
+		dupecube.CFrame = rootpos*CFrame.new(0,-1,-5)
+		dupecube.Parent = workspace
+	end
+	debounce = false
+end)
